eb2fc7b69bd562b9ff1f605281823124ab7b4f4b
Erik Tenecela <eriktenecela13@gmail.com>
2020-01-13 21:18:17 -0500

    redux-setup

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,21 +1,10 @@
-<<<<<<< HEAD
-// import { combineReducers } from "redux";
-// import errorReducer from "./errorReducer";
-// import authReducer from "./authReducer";
-
-// export default combineReducers({
-//   error: errorReducer,
-//   auth: authReducer
-// });
-=======
-import counterReducer from './counter';
-import loggedReducer from './isLogged'
-import { combineReducers } from 'redux';
+import counterReducer from "./counter";
+import loggedReducer from "./isLogged";
+import { combineReducers } from "redux";
 
 const allReducers = combineReducers({
-    counter: counterReducer,
-    isLogged: loggedReducer
+  counter: counterReducer,
+  isLogged: loggedReducer
 });
 
 export default allReducers;
->>>>>>> master
