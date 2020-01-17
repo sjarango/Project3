d0bc46a2deaf38e15c9e0d257cde8d46421c426c
Erik Tenecela <eriktenecela13@gmail.com>
2020-01-16 19:52:25 -0500

    authentication-semi

--- client/src/components/index.js
+++ client/src/components/index.js
@@ -1,5 +1,7 @@
-export { default as Editor } from './Editor';
-export { default as Note } from './Note';
-export { default as Nav } from './Nav';
-export { default as Main } from './Main';
-export { default as Login } from './Login';
+export { default as Editor } from "./Editor";
+export { default as Note } from "./Note";
+export { default as Nav } from "./Nav";
+export { default as Main } from "./Main";
+export { default as Register } from "./Register";
+export { default as Logout } from "./Logout";
+export { default as Login } from "./Login";
