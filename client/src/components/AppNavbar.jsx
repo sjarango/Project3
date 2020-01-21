5336cdd5ee7094c9694542e8535079fb361354de
Erik Tenecela <eriktenecela13@gmail.com>
2020-01-21 18:44:59 -0500

    crud-api

--- client/src/components/AppNavbar.jsx
+++ client/src/components/AppNavbar.jsx
@@ -62,7 +62,7 @@ class AppNavbar extends Component {
       <div>
         <Navbar color="dark" dark expand="sm" className="mb-5">
           <Container>
-            <NavbarBrand href="/">Notes</NavbarBrand>
+            <NavbarBrand href="/notes">Notes</NavbarBrand>
             <NavbarToggler onClick={this.toggle} />
             <Collapse isOpen={this.state.isOpen} navbar>
               <Nav className="ml-auto" navbar>
